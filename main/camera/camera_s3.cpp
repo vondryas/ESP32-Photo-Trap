ffb7a8b6f11c8918193fe51840871b8cd9bc5244
Štěpán Vondráček <xvondr27@stud.fit.vut.cz>
2025-05-02 08:28:21 +0200

    camera update

--- main/camera/camera_s3.cpp
+++ main/camera/camera_s3.cpp
@@ -51,7 +51,7 @@ static camera_config_t camera_config = {
     .pixel_format = PIXFORMAT_JPEG, // YUV422,GRAYSCALE,RGB565,JPEG
     .frame_size = FRAMESIZE_HD,     // 1280x720
 
-    .jpeg_quality = 5, // 0-63 lower number means higher quality
+    .jpeg_quality = 8, // 0-63 lower number means higher quality
     .fb_count = 2,      // if more than one, i2s runs in continuous mode. Use only with JPEG
     .fb_location = CAMERA_FB_IN_PSRAM,
     .grab_mode = CAMERA_GRAB_LATEST,
